1b29e63fb5176922a45d034ac2d946866cb48425
AkashKeshari111 <akashkeshari2722000@gmail.com>
2022-04-30 13:56:32 +0530

    react 1

---
+++ components/navbar.js
@@ -0,0 +1,14 @@
+function navbar(){
+    return ` <h3><a href="/">Home</a></h3>
+      
+  
+   
+       <h3><a href="/jwelery.html">jwelery</a></h3>
+       <h3><a href="/electronics.html">electronics</a></h3>
+       <h3><a href="signup.html">signup</a></h3>
+       <h3><a href="login.html">login</a></h3>   
+    </div>
+    `;
+  }
+
+  export {navbar};
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import {navbar} from '../components/navbar'
+import '../styles/index.css'
+let navbar_div= document.getElementById("navbar")
+navbar_div.innerHTML=navbar();
